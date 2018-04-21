481a11edaed00eb8e7a5051bef81179e593f3864
arthurbumblebee <amakumbi@colby.edu>
2018-04-21 10:40:10 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'party.dart';
+
+void main() => runApp(new Home());
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Pulse',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: new BuildParties(),
+    );
+  }
+}
+
+class BuildParties extends StatefulWidget {
+  @override
+  createState() => new BuildPartiesState();
+}
+
+class BuildPartiesState extends State<BuildParties> {
+  Party p1 =  new Party("Jason's Party", "Foss",["Hangang","Break Bread"], 1002, false);
+  Party p2 =  new Party("Arthur's Party", "Mary Low",["Sleep","until 4"], 1002, true);
+  List<Party> parties = [this.p1, this.p2];
+  final _saved = new Set<String>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Pulse - Party List'),
+      ),
+      body: _buildPartyList(),
+    );
+  }
+
+  Widget _buildPartyList() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          final index = i ~/ 2;
+
+          return _buildPartyRow(parties[index]);
+        },
+        itemCount: 2*parties.length
+    );
+  }
+
+  Widget _buildPartyRow(Party p) {
+    return new ListTile(
+      title: new Text(
+        p.p_name,
+        style: _biggerFont,
+      ),
+
+      onTap: () {
+        _openPlaylist(p.p_playlist);
+      },
+    );
+  }
+
+  void _openPlaylist(List<String> playlist) {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Music Playlist'),
+            ),
+            body: _buildMusicList(playlist),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildMusicList(List<String> playlist) {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          final index = i ~/ 2;
+
+          return _buildMusicRow(playlist[index]);
+        },
+        itemCount: 2*playlist.length
+    );
+  }
+  Widget _buildMusicRow(String music) {
+    final alreadySaved = _saved.contains(music);
+    return new ListTile(
+      title: new Text(
+        music,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(
+              () {
+            if (alreadySaved) {
+              _saved.remove(music);
+            } else {
+              _saved.add(music);
+            }
+          },
+        );
+      },
+    );
+  }
+}
